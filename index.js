b317e9bf369128b2318119b7f0068cb2596a4df9
Kevin Haggerty <kevin.haggerty@havasww.com>
2016-10-20 09:23:42 -0400

    draft of first graphing formatter

--- index.js
+++ index.js
@@ -9,7 +9,45 @@ var wavPlayer = function(stream, spokenText) {
 }
 
 
+var d3_script = '<script src="http://d3js.org/d3.v3.min.js"></script>';
+var dimple_script = '<script src="http://dimplejs.org/dist/dimple.v2.1.6.min.js"></script>'
+
+var getChart = function(data, options) {
+
+  return d3_script + dimple_script + '<div id="" /><script type="text/javascript">' + 
+    'var svg = dimple.newSvg("body", 800, 600);' + 
+    'var data = ' + JSON.stringify(data) + ';' + 
+    'var chart = new dimple.chart(svg, data);' + 
+    'chart.addCategoryAxis("x", "' + options.x + '");' + 
+    'chart.addMeasureAxis("y", "' + options.y + '");' + 
+    'chart.addSeries(null, dimple.plot.' + options.type + ');' + 
+    'chart.draw();' +  '</script>';
+
+var summarizeTones = function (results) {
+  var categories = results.document_tone.tone_categories;
+  var alltones = _.map(categories, function(cat){
+    return cat.tones;
+    // return _.map(cat.tones, function(toneset){
+    //   return toneset.tone_name;
+    // });
+  });
+  return _.compact(_.flatten(alltones));
+  
+}
+var formatToneAnalyzer = function(results, input) {
+    var alltones = summarizeTones(results);
+    var chart = getChart(alltones, {x: "tone_name", y: "score", type: "bar"});
+    console.log(chart);
+}
+
+var sniffFormat = function(results) {
+    if (results.document_tone) return formatToneAnalyzer(results);
+    // others here
+    console.log('no format detected for results:', results);
+}
+
 module.exports = {
-    audio: wavPlayer
+    audio: wavPlayer,
+    render: sniffFormat
 }
 
